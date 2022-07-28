2cd3dd153126bf00ccc585630627071e77f7995c
royal-22 <87497586+royal-22@users.noreply.github.com>
2022-07-28 16:00:11 -0500

    Create caesar.c

---
+++ exercise/lab2/caesar.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void reverse(int key, char *phrase);
+bool validInput(char *str);
+
+int main(int argc, char **argv)
+{
+    char *digits = argv[1];
+
+    if (argc == 2 && validInput(digits))
+    {
+        char text[20];
+
+        printf("Plaintext:   ");
+        scanf("%[^\n]%*c", &text);
+
+        int key = atoi(digits);
+
+        printf("%s", text);
+        //reverse(key, text);
+    }
+    else   
+    {
+        printf("Usage: ./caesar key");
+    }
+}
+
+bool validInput(char *str)
+{
+    int len = strlen(str);
+
+    printf("%i\n", len);
+
+    for(int i=0; i < len; i++)
+    {
+        if (str[i] >= 48 && str[i] <= 57)
+        {
+            continue;
+        }
+        else
+            return false;
+    }
+    return true;
+}
