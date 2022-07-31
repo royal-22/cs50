d446561ddcaf0dd7bb4cbc1f5e8612001d9df40f
royal-22 <87497586+royal-22@users.noreply.github.com>
2022-07-31 09:17:54 -0500

    Update readability.c

--- exercise/lab2/readability.c
+++ exercise/lab2/readability.c
@@ -5,6 +5,7 @@ https://github.com/cs50/labs/tree/2019/fall/credit
 
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int countLetters(char *text);
 int countWords(char *text);
@@ -12,18 +13,40 @@ int countSentence(char *text);
 
 int main(void)
 {
-    char text[100];
+    char text[1000];
     printf("Text:  ");
-    scanf("%s", &text);
+    scanf("%[^\n]%*c", &text);
 
     int letters = countLetters(text);
     int words = countWords(text);
     int sentence = countSentence(text);
 
-    int L = letters / words * 100;
-    int S = sentence / words * 100; 
+    printf("%i\n", letters);
+    
+    printf("%i\n", sentence);
+    
+    printf("%i\n", words);
+
+    float L = ((float)letters / (float)words) * 100;
+    float S = ((float)sentence / (float)words) * 100; 
+
+    /*printf("%i\n", letters/words);
+    printf("%f\n", L);
+    printf("%f\n", S); */
 
     float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    int grade = round(index);
+
+
+    if (grade >= 1 && grade < 16)
+        printf("Grade %i\n", grade);
+
+    else if(grade >= 16)
+        printf("Grade 16+");
+    
+    else
+        printf("Before Grade 1");
 
 }
 
@@ -35,7 +58,7 @@ int countLetters(char *text)
 
     for (int i = 0; i < len; i++)
     {
-        if((text[i] >= 48 && text[i] <= 57) || (text[i] >= 65 && text[i] <= 90))
+        if((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
         {
             count += 1; 
         }
@@ -59,4 +82,18 @@ int countWords(char *text)
     }
     return count;
     
+}
+
+int countSentence(char *text)
+{
+    int count = 0;
+    int len = strlen(text);
+
+    for (int i = 0; i < len; i++)
+    {
+        if ((text[i] == 46) || (text[i] == 63) || (text[i] == 33))
+            count += 1;        
+    }
+    
+    return count;
 }
